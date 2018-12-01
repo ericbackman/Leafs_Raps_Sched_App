99f3855d7621bcbf5f90a250d64b205824b84a2d
ericbackman <eric.backman@sympatico.ca>
2018-12-01 17:23:10 -0500

    xcode app template added

---
+++ Leafs Raps Schedule/Leafs Raps ScheduleTests/Leafs_Raps_ScheduleTests.swift
@@ -0,0 +1,36 @@
+//
+//  Leafs_Raps_ScheduleTests.swift
+//  Leafs Raps ScheduleTests
+//
+//  Created by Eric Backman on 2018-12-01.
+//  Copyright © 2018 Eric Backman. All rights reserved.
+//
+
+import XCTest
+@testable import Leafs_Raps_Schedule
+
+class Leafs_Raps_ScheduleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
